17a53f1398f7cc3c4a5eec08f2263d5bef40ace4
Guillermo Delgado <>
2021-05-24 16:36:50 +0000

    realicé algunos cambios, enfocados al objetivo de la práctica y agregué algunas dudas, ahí me disculpa, y si no me las puede contestar, lo entiendo, muchas gracias

--- main.cpp
+++ main.cpp
@@ -5,12 +5,25 @@ mañana en el transcurso del día si me lo permite la enviaré terminada*/
 
 
 /* el programa mostrará el consumo de 5 dispositivos eléctricos, los cuales son: 
-a-termoducha 
-b-cargador 
-c-bombillo 
-d-pantalla 
-e-micro
+a-termoducha, cosumo de 5500W
+b-cargador de celular, consumo de 60 W
+c-bombillo, consumo de 10W 
+d-pantalla de 50, consumo de 150W
+e-micro, consumo de 1100W
 */
+
+
+/* pude adaptar el código al objetivo de la práctica, y utilicé parte de la programación que vimos en el 
+curso, pero se que no esta perfecta. Lo malo es que el usuario tendría que estar revisando la lista de todos los dispositivos
+ para buscar su potencia y digitarla.
+Me gustaria poder generar una lista, que me jale las potencias automáticamente con solo colocar el 
+nombre del dispositivo, lo investigaré para mostrarselo a mis estudiantes, igual quedo atento algún consejo para realizarlo*/
+
+/* otra cosa que me gustaría saber, investigar o que usted me oriente, es que independientemente del tamaño que yo asigné en el string, que si solo quiero revisar 2 dispositivos, ya salirme del programa, y no tener que digitar toda la lista, como en este caso, que el mínimo lo puse a 5 posiciones*/
+
+
+/* quiero agradecerle por sus clases, por que gracias a ellas aprendí mucho, se que para este código habrá una manera corta de realizarlo, pero para ser principiante en programación y nunca haber programado en c++, siento que haber logrado lo que logré, no esta mal, y es un buen comienzo, bendiciones*/
+
 
 #include <iostream>
 using namespace std;
@@ -18,12 +31,12 @@ using namespace std;
 int main() {  // inicia nuestro programa
 
 //agrego las siguientes variables enteras que necesitaré en mi programa
-int Total_de_horas=0; int entrada=0; int dispositivo_eléctrico;
+int Total_de_consumo=0; int entrada=0; int dispositivo_eléctrico;int potencia;int Total_de_horas;int watts_hora;
 
 string nombres[5];/* el string es para agregar los dispositivos en cadena de texto,
 se le llama nombres y se le asigna un tamaño de 5 */
 
-cout<<"igrese el dispositivo eléctrico a la lista  "<<endl; /*mensaje para 
+cout<<"Igrese el dispositivo eléctrico a la lista  "<<endl; /*mensaje para 
 indicar al usuario que agrgue un dispositivo a la lista, con un salto de 
 línea al final para mejorar la legibilidad*/
 
@@ -41,82 +54,122 @@ i++;                  // sumo en 1 el valor de i, hasta llegar a 4, ya que lo de
 }
 
 cout<< endl <<endl;   // agrego dos saltos de línea, para mejorar la legibilidad
-cout<< "los nombres ingresados son: " << endl;    // el programa muestra los 5 nombres ingresados
+  cout<< "los nombres ingresados son: " << endl;    // el programa muestra los 5 nombres ingresados
 
 a=nombres[0];         // se le asiga a la variable a, el nombre ingresado en la posición 0 
-cout<<a<<endl;        // se muestra el nombre guardado en la variable a, y agrego salto de línea para mejorar la legibilidad         
+  cout<<a<<endl;        // se muestra el nombre guardado en la variable a, y agrego salto de línea para mejorar la legibilidad         
 b=nombres[1];         // se le asiga a la variable b, el nombre ingresado en la posición 1
-cout<<b<<endl;        // se muestra el nombre guardado en la variable b, y agrego salto de línea para mejorar la legibilidad
+  cout<<b<<endl;        // se muestra el nombre guardado en la variable b, y agrego salto de línea para mejorar la legibilidad
 
 c=nombres[2];         // se le asiga a la variable a, el nombre ingresado en la posición 2 
-cout<<c<<endl;        // se muestra el nombre guardado en la variable c, y agrego salto de línea para mejorar la legibilidad         
+  cout<<c<<endl;        // se muestra el nombre guardado en la variable c, y agrego salto de línea para mejorar la legibilidad         
 d=nombres[3];         // se le asiga a la variable b, el nombre ingresado en la posición 3
-cout<<d<<endl;        // se muestra el nombre guardado en la variable d, y agrego salto de línea para mejorar la legibilidad
+  cout<<d<<endl;        // se muestra el nombre guardado en la variable d, y agrego salto de línea para mejorar la legibilidad
 
 e=nombres[4];         // se le asiga a la variable b, el nombre ingresado en la posición 4
-cout<<e<<endl;        // se muestra el nombre guardado en la variable e, y agrego salto de línea para mejorar la legibilidad
+  cout<<e<<endl;        // se muestra el nombre guardado en la variable e, y agrego salto de línea para mejorar la legibilidad
 
-cout<<endl<<endl<<endl; // se generó 3 espacios con el retorno de carro
+     cout<<endl<<endl<<endl; // se generó 3 espacios con el retorno de carro
 
 // ahora ingresaremos al programa las horas de cosumo de cada dispositivo
 cout << "Escriba la cantidad de horas de consumo del siguiente dipositivo: \n" ;
-cout<<a<<endl; 
+  cout<<a<<endl; 
 cin>> entrada;       // ingreso las horas de consumo para el dispositivo mostrado en la varible a
-cout <<"Las horas de consumo del dispositivo eléctrico  " <<a<< "  son de: "<<endl;cout<<entrada<< " horas"; // el programa muestra las horas de consumo ingresadas en el paso anterior
-cout <<"\n""\n""\n";  // se generó 3 espacios con el retorno de carro
+  cout <<"Las horas de consumo del dispositivo eléctrico  " <<a<< "  son de: "<<endl;
+    cout<<entrada<< " horas"; // el programa muestra las horas de consumo ingresadas en el paso anterior
+      cout <<"\n""\n""\n";  // se generó 3 espacios con el retorno de carro
 
+cout <<"Ingrese la potencia en Watts que necesita el dispositivo " <<a<< " para su funcionamiento" <<endl ; // potencia en watts de la termoducha
+cin>>potencia;
+  cout <<"La potencia de consumo del dispositivo eléctrico  " <<a<< "  es de: " <<endl;  // potencia en watts de la termoducha
+    cout<<potencia<< " watts" <<endl<<endl;
+watts_hora=entrada*potencia;  // se multiplica la potencia por la cantidad de horas, y obtengo el gasto por horas
 
-// se calcula el total de horas y se van contando las horas de todos los dispositivos
-Total_de_horas= Total_de_horas+entrada;    // en este caso el total es el mismo valor que el agregado en la variable a, ya que es el primero, y la variable entrada se inicia en 0
-cout <<"EL TOTAL DE HORAS DE CONSUMO EN EL HOGAR SON DE: "<<endl;
-cout<<Total_de_horas<<" horas";     // se muestra el total de horas calculadas en el paso anterios
-cout <<"\n""\n""\n";
+Total_de_consumo= Total_de_consumo+watts_hora;    // se realiza un contador, para estar guardando la potencia total
+  cout <<"EL TOTAL DE CONSUMO EN LA CASA ES DE: "<<endl;
+    cout<<Total_de_consumo<<" watts";     // se muestra el total del gasto watt-hora calculados en el paso anterios
+      cout <<"\n""\n""\n";
 
 
 // para los siguientes pasos la lógica es la misma que la anterios,por lo que mejor no voy a explicar el código, para no llenar el programa con más palabras
 
 cout << "Escriba la cantidad de horas de consumo del siguiente dipositivo: \n" ;
-cout<<b<<endl;
+  cout<<b<<endl;
 cin>> entrada ;
-cout <<"Las horas de consumo del dispositivo eléctrico  " <<b<< "  son de: "<<endl;cout<<entrada<< " horas";
-cout <<"\n""\n""\n";
-Total_de_horas= Total_de_horas+entrada; 
-cout <<"EL TOTAL DE HORAS DE CONSUMO EN EL HOGAR SON DE: "<<endl;
-cout<<Total_de_horas<<" horas";
-cout <<"\n""\n""\n";
+  cout <<"Las horas de consumo del dispositivo eléctrico  " <<b<< "  son de: "<<endl;
+    cout<<entrada<< " horas";
+      cout <<"\n""\n""\n";
+
+cout <<"Ingrese la potencia en Watts que necesita el dispositivo " <<b<< " para su funcionamiento" <<endl ;
+cin>>potencia;
+  cout <<"La potencia de consumo del dispositivo eléctrico  " <<b<< "  es de: " <<endl;
+    cout<<potencia<< " watts" <<endl<<endl;
+watts_hora=entrada*potencia;
+
+Total_de_consumo= Total_de_consumo+watts_hora;
+cout <<"EL TOTAL DE CONSUMO EN LA CASA ES DE: "<<endl;
+  cout<<Total_de_consumo<<" watts";     
+    cout <<"\n""\n""\n";
 
 
 cout << "Escriba la cantidad de horas de consumo del siguiente dipositivo: \n" ;
-cout<<c<<endl;
+  cout<<c<<endl;
 cin>> entrada ;
-cout <<"Las horas de consumo del dispositivo eléctrico  " <<c<< "  son de: "<<endl;cout<<entrada<< " horas";
-cout <<"\n""\n""\n";
-Total_de_horas= Total_de_horas+entrada; 
-cout <<"EL TOTAL DE HORAS DE CONSUMO EN EL HOGAR SON DE: "<<endl;
-cout<<Total_de_horas<<" horas";
-cout <<"\n""\n""\n";
+  cout <<"Las horas de consumo del dispositivo eléctrico  " <<c<< "  son de: "<<endl;
+    cout<<entrada<< " horas";
+      cout <<"\n""\n""\n";
+
+cout <<"Ingrese la potencia en Watts que necesita el dispositivo " <<c<< " para su funcionamiento" <<endl ;
+cin>>potencia;
+  cout <<"La potencia de consumo del dispositivo eléctrico  " <<c<< "  es de: " <<endl;
+    cout<<potencia<< " watts" <<endl<<endl;
+watts_hora=entrada*potencia;
+
+Total_de_consumo= Total_de_consumo+watts_hora;
+cout <<"EL TOTAL DE CONSUMO EN LA CASA ES DE: "<<endl;
+  cout<<Total_de_consumo<<" watts";     
+    cout <<"\n""\n""\n";
+
 
 
 cout << "Escriba la cantidad de horas de consumo del siguiente dipositivo: \n" ;
-cout<<d<<endl;
+  cout<<d<<endl;
 cin>> entrada ;
-cout <<"Las horas de consumo del dispositivo eléctrico  " <<d<< "  son de: "<<endl;cout<<entrada<< " horas";
-cout <<"\n""\n""\n";
-Total_de_horas= Total_de_horas+entrada; 
-cout <<"EL TOTAL DE HORAS DE CONSUMO EN EL HOGAR SON DE: "<<endl;
-cout<<Total_de_horas<<" horas";
-cout <<"\n""\n""\n";
+  cout <<"Las horas de consumo del dispositivo eléctrico  " <<d<< "  son de: "<<endl;
+    cout<<entrada<< " horas";
+      cout <<"\n""\n""\n";
+
+cout <<"Ingrese la potencia en Watts que necesita el dispositivo " <<d<< " para su funcionamiento" <<endl ;
+cin>>potencia;
+  cout <<"La potencia de consumo del dispositivo eléctrico  " <<d<< "  es de: " <<endl;
+    cout<<potencia<< " watts" <<endl<<endl;
+watts_hora=entrada*potencia;
+
+Total_de_consumo= Total_de_consumo+watts_hora;
+cout <<"EL TOTAL DE CONSUMO EN LA CASA ES DE: "<<endl;
+  cout<<Total_de_consumo<<" watts";     
+    cout <<"\n""\n""\n";
+
+
+
 
 
 cout << "Escriba la cantidad de horas de consumo del siguiente dipositivo: \n" ;
-cout<<e<<endl;
+  cout<<e<<endl;
 cin>> entrada ;
-cout <<"Las horas de consumo del dispositivo eléctrico  " <<e<< "  son de: "<<endl;cout<<entrada<< " horas";
-cout <<"\n""\n""\n";
-Total_de_horas= Total_de_horas+entrada; 
-cout <<"EL TOTAL DE HORAS DE CONSUMO EN EL HOGAR SON DE: "<<endl;
-cout<<Total_de_horas<<" horas";
-cout <<"\n""\n""\n";
+  cout <<"Las horas de consumo del dispositivo eléctrico  " <<e<< "  son de: "<<endl;
+    cout<<entrada<< " horas";
+      cout <<"\n""\n""\n";
 
+cout <<"Ingrese la potencia en Watts que necesita el dispositivo " <<e<< " para su funcionamiento" <<endl ;
+cin>>potencia;
+  cout <<"La potencia de consumo del dispositivo eléctrico  " <<e<< "  es de: " <<endl;
+    cout<<potencia<< " watts" <<endl<<endl;
+watts_hora=entrada*potencia;
+
+Total_de_consumo= Total_de_consumo+watts_hora;
+cout <<"EL TOTAL DE CONSUMO EN LA CASA ES DE: "<<endl;
+  cout<<Total_de_consumo<<" watts";     
+    cout <<"\n""\n""\n";
 
 }
